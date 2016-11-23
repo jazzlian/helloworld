f5685285685b11064a05a1e4e2f291d49a77b7ab
Myoung Sig Youn <jazzlian@gmail.com>
2016-11-23 11:22:15 +0900

    Changes to be committed: 	new file:   cpp/helloworld.cpp 	new file:   cs/hellowold.cs 	new file:   pascal/helloworld.pas

---
+++ cpp/helloworld.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include <iostream>
+
+using namespace std;
+
+int
+main(int argc, char **argv)
+{
+	std::cout << "hello world\n";
+
+	return 0;
+}
---
+++ pascal/helloworld.pas
@@ -0,0 +1,9 @@
+program Hello;
+var MyMessage: string;
+
+begin
+        MyMessage := 'hello world!';
+        
+        writeln(MyMessage);
+        readln();
+end.
