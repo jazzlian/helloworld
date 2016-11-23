4dcfbb8c76a3fbac90d282ef9663639642344aea
Myoung Sig Youn <jazzlian@gmail.com>
2016-11-23 11:09:18 +0900

    Changes to be committed: 	new file:   go/helloworld.go 	new file:   java/helloworld.java

---
+++ java/helloworld.java
@@ -0,0 +1,11 @@
+import java.lang.String;
+import java.lang.System;
+import java.lang.Exception;
+
+public final class  helloworld
+{
+	public static void main(String[] args) throws Exception {
+		
+		System.out.println("hello world");
+	}
+}
