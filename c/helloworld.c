36ba6f5c9b0fe9c96f4dce0f8903891065be47eb
Myoung Sig Youn <jazzlian@gmail.com>
2017-12-13 14:49:37 +0900

    2017-12-13

--- c/helloworld.c
+++ c/helloworld.c
@@ -1,10 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define BRIGHT 1
+#define RED 31
+#define BG_BLACK 40
+
 int
 main(int argc, char **argv)
 {
 
-    printf("%c[%d;%dmHello World%c[%dm\n",27,1,33,27,0);
+    printf("%c[%d;%d;%dm Hello World", 0x1B, BRIGHT,RED,BG_BLACK);
 	return EXIT_SUCCESS;
 }
