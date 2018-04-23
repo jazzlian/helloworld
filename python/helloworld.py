262824aa47de44b41bfb4ff30346dc7b31fd27d4
윤명식 <jazzlian@gmail.com>
2018-04-23 02:46:50 +0000

    2018.04.23:  Modify Hello Class

--- python/helloworld.py
+++ python/helloworld.py
@@ -7,6 +7,11 @@ class Hello():
         self.names = 'Myoungsig '
     def sayHello(self):
         print(self.names + ' Hello World!')
+    def __del__(self):
+        print(self.names + ' I Will back')
+        
 if __name__ == '__main__':
         myHello = Hello()
         myHello.sayHello()
+        
+        sys.exit()
